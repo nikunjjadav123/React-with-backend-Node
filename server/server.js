fb9442b56366b93c10efbbe3b0c4c8c3e7b56696
Nikunj Jadav <dirensj@gmail.com>
2024-06-12 16:21:50 +0530

    Added server.js script to connect backend to front end again

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/message", (req, res) => {
-  res.json({ message: "This is React With NodePooja" });
+  res.json({ message: "This is React With Node" });
 });
 
 app.listen(8000, () => {

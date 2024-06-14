9026144a9ed7e3c6fd07c54e5903c75590d2e396
Nikunj Jadav <dirensj@gmail.com>
2024-06-14 16:32:50 +0530

    added some libraries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+import { createConnection } from 'mysql';
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/message", (req, res) => {
-  res.json({ message: "This is React With Node" });
+const connection = createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'zara'
 });
 
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to MySQL as ID ' + connection.threadId);
 });
+
